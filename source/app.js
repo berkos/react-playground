9f6a0918696dc608e2d54795b94dab88be984fc6
Antonis Berkakis <aberkakis@gmail.com>
2017-07-28 21:16:43 +0100

    Use a React component

---
+++ source/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render(){
+    return <div className="jumbotron jumbotron-fluid text-sm-center" style={{backgroundImage: 'url(img/nature.jpg)', color: 'white'}}>
+      <div className="container">
+        <h1>Build React.js User Interfaces</h1>
+        <p>with Bootstrap and SASS</p>
+        <p><a className="btn btn-primary" href="http://www.google.com" target="_blank">Google things</a></p>
+      </div>
+    </div>;
+  }
+}
