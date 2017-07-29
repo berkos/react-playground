d86265b9dd5e7b2ee84c1adb8157dbf2e2815384
Antonis Berkakis <aberkakis@gmail.com>
2017-07-29 12:19:56 +0100

    export classify

--- source/bootstrap/button.js
+++ source/bootstrap/button.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import {classify} from './utils';
 
 export default class Button extends React.Component {
-  classify(key, value) {
-    if(value[0] === '-') value = key + value;
-    value =  value.split(' -').join(` ${key}-`)
-    return `${key} ${value}`;
-  }
+
 
   render() {
     const Tag = this.props.href ? 'a' : 'button';
-    const className =  this.classify('btn', this.props.className) + (this.props.disabled? ' disabled' : '');
+    const className =  classify('btn', this.props.className) + (this.props.disabled? ' disabled' : '');
 
     return <Tag {...this.props} className={className}>Google things</Tag>;
   }
