8b68495bd9f0c02da4a2726c4edd67948a8c7e98
Antonis Berkakis <aberkakis@gmail.com>
2017-07-30 23:56:43 +0100

    Set up click and constructor for App

--- source/app.js
+++ source/app.js
@@ -5,15 +5,27 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(e) {
+    console.log(this);
+  }
+
   render() {
-    return <ReactCSSTransitionGroup transitionName="app" transitionAppear={true} transitionAppearTimeout={200} transitionEnterTimeout={100} transitionLeaveTimeout={300}><Jumbotron className="-fluid text-sm-center custom" containerFluid={true}>
-        <h1>Build React.js User Interfaces</h1>
-        <p>with Bootstrap and SASS</p>
-        <p>
-          <Button className="-primary -success" href="http://www.google.com" target="_blank" label="Discover things"></Button>
-          <Button className="-danger -sm" label="Discover things2">ssss</Button>
-        </p>
-      <Button className="-primary -md -block">Discover things3</Button>
+    return <ReactCSSTransitionGroup transitionName="app" transitionAppear={true} transitionAppearTimeout={200}
+      transitionEnterTimeout={100} transitionLeaveTimeout={300}><Jumbotron className="-fluid text-sm-center custom"
+      containerFluid={true}>
+      <h1>Build React.js User Interfaces</h1>
+      <p>with Bootstrap and SASS</p>
+      <p>
+        <Button className="-primary -success" href="http://www.google.com" target="_blank"
+          label="Discover things"></Button>
+        <Button className="-danger -sm" label="Discover things2">ssss</Button>
+      </p>
+      <Button onClick={this.onClick} className="-primary -md -block">Discover things3</Button>
     </Jumbotron></ReactCSSTransitionGroup>;
   }
 }
