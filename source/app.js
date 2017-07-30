52229062be765418d00b649df3b920e5a312c4fb
Antonis Berkakis <aberkakis@gmail.com>
2017-07-30 23:03:30 +0100

    React transition

--- source/app.js
+++ source/app.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 export default class App extends React.Component {
   render() {
-    return <Jumbotron className="-fluid text-sm-center custom" containerFluid={true}>
+    return <ReactCSSTransitionGroup transitionName="app" transitionAppear={true} transitionAppearTimeout={200} transitionEnterTimeout={100} transitionLeaveTimeout={300}><Jumbotron className="-fluid text-sm-center custom" containerFluid={true}>
         <h1>Build React.js User Interfaces</h1>
         <p>with Bootstrap and SASS</p>
         <p>
-          <Button className="-primary -success" href="http://www.google.com" target="_blank" disabled/>
-          <Button className="-danger -sm" disabled/>
+          <Button className="-primary -success" href="http://www.google.com" target="_blank"/>
+          <Button className="-danger -sm"/>
         </p>
-        <Button className="-primary -md -block" disabled/>
-    </Jumbotron>;
+        <Button className="-primary -md -block"/>
+    </Jumbotron></ReactCSSTransitionGroup>;
   }
 }
