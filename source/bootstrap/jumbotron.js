58c494c3ccd3e78204723fa3e0fc4727cf1b4dcf
Antonis Berkakis <aberkakis@gmail.com>
2017-07-30 14:06:37 +0100

    Small fix in Jumbotron

--- source/bootstrap/jumbotron.js
+++ source/bootstrap/jumbotron.js
@@ -4,14 +4,19 @@ import Container from "./container";
 
 export default class Jumbotron extends React.Component {
   render() {
+    const CSS_NAME = 'jumbotron';
     const props = Object.assign({}, this.props);
+    props.className = classify(CSS_NAME, props.className);
     const containerFluid = props.containerFluid;
     delete props.containerFluid;
 
-    return <div {...props} className={classify('jumbotron', props.className)}>
-      <Container fluid={containerFluid}/>
-      {this.props.children}
-      <Container/>
-    </div>;
+    const body = props.className.includes(CSS_NAME + '-fluid') ?
+      <Container fluid={containerFluid}> {this.props.children} </Container>
+      :
+      this.props.children;
+
+    return <div {...props}>
+      {body};
+    </div>
   }
 }
